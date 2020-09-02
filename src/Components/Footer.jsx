678ca78a6f44df299beaa9ffe98ab0e5c0ab15b9
Annie Davis <anniecdavis27@gmail.com>
2020-09-01 22:49:45 -0400

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 function Footer(props) {
-    return (
-      <div className="Foot">
-        <h1>This is Footer</h1>
+  return (
+    <div className="Foot">
+      <div className="footer-content">
+        <img
+          src="https://live.staticflickr.com/65535/50296512262_0c1f077dfc_o.jpg"
+          alt="author"
+          className="annie-pic"
+        ></img>
+        <section className="footer-text">
+          <h2>About the Author</h2>
+          <p>
+            Deel op kilo te voor uren al hier land de. Atjehers doodende
+            bepaalde men per gestookt. Hanteeren in aankoopen af ontginnen
+            resideert voorspoed de ze. Nu en of behalen kintyas op ontdekt. Dan
+            diezelfde hun brandstof men ver aardschok. Ten goa uitstekend
+            traliewerk initiatief. Ontgonnen die ingericht het vertoonen
+            brandstof aangelegd.{" "}
+          </p>
+        </section>
       </div>
-    );
-  }
-  
-  export default Footer;
+      <div className="copyright">
+        <p className="copy-p">
+          2020 © all rights reserved
+          <br />
+          Created by Annie Chase Davis
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
